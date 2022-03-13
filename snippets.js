48aed01baa29883e1d0df3898179a135ca403df4
Woodstock <victorhe97@gmail.com>
2022-03-12 23:01:02 -0500

    Bug found with the img links

--- snippets.js
+++ snippets.js
@@ -1,35 +1,23 @@
-/*
-- Go to kohls.com and add at least 2 products to your cart. Then return to the home page.
-- Write a JavaScript snippet that can be run in the console of the browser that does the following:
-  - Extracts the number of items in the cart, the cart total, and the item images from the page. Store them in variables.
-  - Creates a trigger that activates when the user scrolls into the bottom 10% of the page.
-  - The trigger should show a centered overlay on top of the site that displays the information gathered above and two buttons:
-    - one button should close the overlay
-    - the other should take the user to the cart page
-  - Behind the overlay add a semi­-transparent black background that obscures the site.
-  - The overlay should be able to trigger multiple times if dismissed.
-- The overlay should have a style consistent with the website. Design matters.
-*/
-
-// extracts number of items in the cart
+/* LOGIC FOR MODAL CONTENT */
 const getNumItemsInCart = () => {
   let numItemsInCart = $('.number-items.boss-number-items.nonzero-items')[0]
     .innerHTML;
   return numItemsInCart;
 };
 
-// extracts the cart total
 const calculateCartTotal = () => {
   let subTotal = $('.subtotal')[0].innerHTML;
   return subTotal;
 };
 
-// extracts the item images from the page
-// to make this better, I think we can use a JS object
-// that way we can hold the key (name of product)
-// and value (img link)
-// add a check to determine if there are any items in cart anyways
-// we should return a more descriptive message instead of an undefined
+/*
+- extracts the item images from the page
+- to make this better, I think we can use a JS object. That way we can hold the key (name of product) and value (img link) add a check to determine if there are any items in cart anyways
+- we should return a more descriptive message instead of an undefined
+
+POSSIBLE BUG
+- need to click on the cart on the homepage before the image links actually load in the popup modal :(
+*/
 const extractCartItemImages = () => {
   let items = $('.kas-newpb-product-image');
   let imgItemsLength = items.length;
